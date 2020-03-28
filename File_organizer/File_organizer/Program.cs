744ca12d197a602e547599cd5ed8497abd33c122
이성현 <dljdgus1211@naver.com>
2020-03-29 00:03:01 +0900

    Added get date code

--- File_organizer/File_organizer/Program.cs
+++ File_organizer/File_organizer/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
 using ImageMagick;
 using System.Diagnostics;
@@ -11,6 +7,7 @@ namespace File_organizer
 {
     class Program
     {
+        // timer
         static public Stopwatch _stopwatch = new Stopwatch();
 
         static void Main(string[] args)
@@ -23,10 +20,13 @@ namespace File_organizer
             _stopwatch.Stop();
 
             Console.WriteLine("총 걸린 시간은 : " + _stopwatch.Elapsed.TotalSeconds.ToString() + "sec");
-            //var info = new FileInfo(srcDirPath);
-            //Console.WriteLine(info);
         }
 
+        /// <summary>
+        /// 저장할 경로의 디렉토리를 생성하고 소스 디렉토리 파일을 처리
+        /// </summary>
+        /// <param name="srcDirPath"></param>
+        /// <param name="trgDirPath"></param>
         static private void OnProcess(string srcDirPath, string trgDirPath)
         {
             DirectoryInfo srcDirInfo = new DirectoryInfo(srcDirPath);
@@ -37,12 +37,18 @@ namespace File_organizer
 
             foreach(FileInfo file in srcDirInfo.GetFiles())
             {
-                FileByDate(file.FullName, trgDirPath);
-            }
+                FileByDateTimeOriginal(file.FullName, trgDirPath);
 
+                FileProfileCheck(file.FullName);
+            }
         }
 
-        static private void FileByDate(string filePath, string trgDirPath)
+        /// <summary>
+        /// 찍은 날짜 기준으로 폴더 생성 후 파일 분류
+        /// </summary>
+        /// <param name="filePath">파일 경로</param>
+        /// <param name="trgDirPath">저장 경로</param>
+        static private void FileByDateTimeOriginal(string filePath, string trgDirPath)
         {
             using (MagickImage img = new MagickImage(filePath))
             {
@@ -63,6 +69,7 @@ namespace File_organizer
                 string[] dateTimes = dateTime.Split();
                 string date = dateTimes[0].Replace(":", ".");
                 string saveDirPath = Path.Combine(trgDirPath, date);
+
                 DirectoryInfo saveDir = new DirectoryInfo(saveDirPath);
                 FileInfo imgFile = new FileInfo(filePath);
 
@@ -71,6 +78,48 @@ namespace File_organizer
 
                 string saveFilePath = Path.Combine(saveDirPath, Path.GetFileName(filePath));
                 imgFile.MoveTo(saveFilePath);
+            }
+        }
+
+        static private void FileByDate(string filePath, string trgDirPath)
+        {
+            using (MagickImage img = new MagickImage(filePath))
+            {
+                string dateTime = null;
+
+                foreach (IExifValue value in img.GetExifProfile().Values)
+                {
+                    if (value.Tag.ToString() == "DateTime")
+                    {
+                        Console.WriteLine("{0}({1}): {2}", value.Tag, value.DataType, value.ToString());
+                        dateTime = value.ToString();
+                    }
+                }
+
+                if (dateTime == null)
+                    return;
+
+                string[] dateTimes = dateTime.Split();
+                string date = dateTimes[0].Replace(":", ".");
+                string saveDirPath = Path.Combine(trgDirPath, date);
+
+                DirectoryInfo saveDir = new DirectoryInfo(saveDirPath);
+                FileInfo imgFile = new FileInfo(filePath);
+
+                if (!saveDir.Exists)
+                    saveDir.Create();
+
+                string saveFilePath = Path.Combine(saveDirPath, Path.GetFileName(filePath));
+                imgFile.MoveTo(saveFilePath);
+            }
+        }
+
+        static private void FileProfileCheck(string filePath)
+        {
+            using (MagickImage img = new MagickImage(filePath))
+            {
+                foreach (IExifValue value in img.GetExifProfile().Values)
+                    Console.WriteLine("{0}({1}): {2}", value.Tag, value.DataType, value.ToString());
             }
         }
     }
